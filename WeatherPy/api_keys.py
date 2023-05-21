4334d2bf17f803d2f91627ccbb0792fe3fb52b2d
Stephanie Loomer <sdloomer@gmail.com>
2023-05-21 12:46:47 -0700

    configured .gitignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "19897030eeef49d10e340b8b4b8f3ec3"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "24bcda0786924fb1acabcea0cfe5a368"
